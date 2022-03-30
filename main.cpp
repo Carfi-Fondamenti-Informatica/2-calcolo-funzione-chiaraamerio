d801a1d1926aea5964450511e3e6307881db2d16
chiaraamerio <100305899+chiaraamerio@users.noreply.github.com>
2022-03-30 15:30:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
-   return 0;
+   float a=0, b=0, x=0, y=0, z=0;
+   cout<<"inserire 4 valori reali"<<endl;
+   cin>>a>>b>>x>>y;
+   
+   if(x<0 && y>0){
+      z=(a*x)-(b*y);
+      cout<<z<<endl;
+      
+   }else if (x>=0 && y<=0) {
+      z=(a*x*x)-(b*y);
+      cout<<z<<endl; 
+      
+   }else {
+      z=(a*x)+(b*y*y);
+      cout<<z<<endl;
+   }
+      
+return 0;
 }
